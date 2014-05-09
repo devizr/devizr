24a8d240bc46ebccfb6fc3c91c07c8a4a920193b
Uli Preuss <me@ulipreuss.eu>
2014-05-10 01:15:12 +0200

    added new test method: type createElement

--- src/devizr.app.js
+++ src/devizr.app.js
@@ -56,6 +56,9 @@
       }
 
       function getIframeInterfaces(iface){
+			  if(typeof iface === 'string') {
+			  	return iframe.contentDocument.createElement(iface);
+			  }
         switch(iface){
         case window : return iframe.contentWindow;
         case document : return iframe.contentDocument;
